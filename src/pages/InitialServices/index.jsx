8eb8b3eac8eb270cb382ec05832d4a84b1be1bc0
diegoberselli <diegoberselliz@gmail.com>
2022-03-16 23:15:19 -0300

    feat: add header on initialpage

--- src/pages/InitialServices/index.jsx
+++ src/pages/InitialServices/index.jsx
@@ -1,7 +1,42 @@
-const InitialServices = () => {
-    return (
-        <h1>InitialServices</h1>
-    )
-}
+import { Container, Title } from "./styled";
+import { Box, TextInput, Menu } from "grommet";
+import { useState } from "react";
+import { Search } from "grommet-icons";
+import { User } from "grommet-icons";
 
-export default InitialServices
+const InitialServices = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <Container>
+      <div>
+        <Menu
+          label="Menu"
+          color="brand"
+          items={[
+            { label: "First Action", onClick: () => {} },
+            { label: "Second Action", onClick: () => {} },
+          ]}
+        />
+        <User />
+      </div>
+
+      <Title>Jobinhos</Title>
+
+      <Box fill align="center" justify="start" pad="large">
+        <Box width="large" gap="medium">
+          <TextInput
+            color="brand"
+            icon={<Search />}
+            reverse
+            placeholder="O que você procura? ..."
+            plain={false}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default InitialServices;
