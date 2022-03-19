2f5302d56fa943a042cf523f18ebdcd1ed440520
Felipe <felipesindeaux@outlook.com>
2022-03-18 21:22:39 -0300

    feat: register-api

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,59 +3,78 @@ import { FormContainer } from "./styled";
 import { RegisterContainer } from "./styled";
 import Header from "../../components/Header";
 import img from "../../assets/img2.png";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import api from "../../services/api";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [type, setType] = useState("");
+  const [auth, setAuth] = useState(false);
+  const history = useHistory();
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Campo Obrigatório"),
+    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    type: yup.string().required("Campo Obrigatório"),
+    password: yup.string().required("Campo obrigatório"),
+    password_confirm: yup
+      .string()
+      .oneOf([yup.ref("password")], "Senha diferentes")
+      .required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const submitData = (data) => {
+    delete data.password_confirm;
+    const newData = {
+      ...data,
+      img: "http://www.def.uem.br/arquivos/imagens/fotos-dos-docentes/semimagemavatar.png/image_preview",
+    };
+    api
+      .post("/register", newData)
+      .then((response) => {
+        toast.success('Registrado com sucesso!')
+        history.push('/login')
+      })
+
+      .catch((_) => {
+        toast.error("Ops, algo deu errado!");
+      });
+  };
 
   return (
     <>
-      <Header page={'user'} />
+      <Header page={"user"} />
       <RegisterContainer>
         <div className="welcome-container">
-          <img src={img} alt='' />
+          <img src={img} alt="registerImage" />
           <h2 className="desktop">Seja seu próprio chefe</h2>
         </div>
-        <FormContainer>
+        <FormContainer onSubmit={handleSubmit(submitData)}>
           <p>Nome</p>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <input {...register("name")} />
           <p>Email</p>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <input {...register("email")} />
           <p>Eu sou...</p>
-          <select
-            name=""
-            id=""
-            value={type}
-            onChange={(e) => setType(e.target.value)}
-          >
-            <option value="consumidor">Consumidor</option>
-            <option value="Prestador de serviços">Prestador de serviços</option>
+          <select {...register("type")}>
+            <option value="Consumidor">Consumidor</option>
+            <option value="Prestador">Prestador de serviços</option>
           </select>
           <p>Senha</p>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" {...register("password")} />
           <p>Confirmar senha</p>
-          <input
-            type="password"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-          />
+          <input type="password" {...register("password_confirm")} />
 
-          <button>Registrar</button>
+          <button type="submit">Registrar</button>
         </FormContainer>
       </RegisterContainer>
     </>
