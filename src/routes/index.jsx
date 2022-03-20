fef8e49b0c4856c46898df2c915e8470c928b618
Felipe <felipesindeaux@outlook.com>
2022-03-20 12:44:30 -0300

    fix: routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,21 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Route from "./route";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Pendings from "../pages/Pendings";
 import Profile from "../pages/Profile";
 import InitialServices from "../pages/InitialServices";
-import MyServices from "../pages/MyServices";
+import MyServices from '../pages/MyServices'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={InitialServices} />
-        <Route path="/pendings" component={Pendings}/>
-        <Route path="/profile" component={Profile}/>
+        <Route path="/pendings" isPrivate component={Pendings}/>
+        <Route path="/profile" isPrivate component={Profile}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
-        <Route path="/MyServices" component={MyServices}/>
-    </Switch>    
+        <Route path="/myServices" component={MyServices}/>
+    </Switch>
 )
 
 export default Routes
