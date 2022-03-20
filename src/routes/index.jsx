94c22f924a1d994722524ac7b7cadacf1bd9184d
Felipe <felipesindeaux@outlook.com>
2022-03-20 12:30:24 -0300

    feat: authentication

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Route from "./route";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Pendings from "../pages/Pendings";
@@ -8,8 +9,8 @@ import InitialServices from "../pages/InitialServices";
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={InitialServices} />
-        <Route path="/pendings" component={Pendings}/>
-        <Route path="/profile" component={Profile}/>
+        <Route path="/pendings" isPrivate component={Pendings}/>
+        <Route path="/profile" isPrivate component={Profile}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
     </Switch>
