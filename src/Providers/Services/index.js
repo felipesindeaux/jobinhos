eaad2dba0a76621f0554bb0c0267cd5e036278c0
Felipe <felipesindeaux@outlook.com>
2022-03-20 17:27:45 -0300

    feat: filter

--- src/Providers/Services/index.js
+++ src/Providers/Services/index.js
@@ -7,18 +7,42 @@ export const ServicesContext = createContext();
 export const ServicesProvider = ({ children }) => {
   const { userInfo } = useContext(UserContext);
   const [services, setServices] = useState();
+  const [backup, setBackup] = useState(services);
   const [hireService, setHireService] = useState();
 
   useEffect(() => {
-    api.get("/services").then((response) => setServices(response.data));
+    api.get("/services").then((response) => {
+      setServices(response.data);
+      setBackup(response.data);
+    });
   }, []);
 
   const updateServices = () => {
-    api.get("/services").then((response) => setServices(response.data));
+    api.get("/services").then((response) => {
+      setServices(response.data);
+      setBackup(response.data);
+    });
   };
 
   const getUserServices = () => {
     return services.filter((service) => service.userId === userInfo.id);
+  };
+
+  const filterServices = (value) => {
+    setServices(backup);
+
+    const filteredServices = backup.filter(
+      (service) =>
+        service.name.toLowerCase().includes(value.toLowerCase()) ||
+        service.title.toLowerCase().includes(value.toLowerCase()) ||
+        service.tags.join().includes(value[0].toUpperCase() + value.substr(1))
+    );
+
+    if (value === "") {
+      setServices(backup);
+    } else {
+      setServices(filteredServices);
+    }
   };
 
   return (
@@ -29,6 +53,7 @@ export const ServicesProvider = ({ children }) => {
         setHireService,
         updateServices,
         getUserServices,
+        filterServices,
       }}
     >
       {children}
