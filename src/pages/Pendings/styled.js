bab5e3b965425e9b1caf2c706bf333ce5577a531
Felipe <felipesindeaux@outlook.com>
2022-03-20 12:39:19 -0300

    fix: conflicts

--- src/pages/Pendings/styled.js
+++ src/pages/Pendings/styled.js
@@ -1,7 +1,36 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin-top: 42px;
+
+  h2 {
+    color: var(--brand);
+    font-size: 36px;
+    width: 200px;
+    margin-bottom: 12px;
+  }
+
+  h3 {
+    color: var(--dark-6);
+    font-size: 24px;
+  }
+
+  @media (min-width: 1000px){
+    flex-direction: row;
+  }
+`;
+
+export const Greetings  = styled.div`
+  padding-left: 22px;
+  margin-bottom: 10px;
+
+  @media (min-width: 1000px){
+    padding-left: 100px;
+  }
 `
+
+export const OpacityContainer = styled.div`
+  opacity: ${(props) => props.opacity};
+`;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Pendings from "../pages/Pendings";
+import Profile from "../pages/Profile";
 import InitialServices from "../pages/InitialServices";
 import MyServices from "../pages/MyServices";
 
@@ -9,6 +10,7 @@ const Routes = () => (
     <Switch>
         <Route exact path="/" component={InitialServices} />
         <Route path="/pendings" component={Pendings}/>
+        <Route path="/profile" component={Profile}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
         <Route path="/MyServices" component={MyServices}/>
