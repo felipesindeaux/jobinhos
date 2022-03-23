8658261fdcdeaa34657cd10dc039e756b3302b46
diegoberselli <diegoberselliz@gmail.com>
2022-03-23 18:34:03 -0300

    feat: add component navBar and styled

---
+++ src/components/MobileNav/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Container} from "./styled";
+
+
+
+const NavBar = ({options}) => {
+  return (
+    <Container >
+        <nav>
+            <div id="menuToggle">
+              <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+            <ul id="menu">
+                {options.map((item) => 
+                <li><button onClick={item.onClick}>{item.label}</button></li>
+                )}
+            </ul>
+           </div>
+        </nav>
+    </Container>
+  );
+};
+
+export default NavBar
