061d38f914681c1294c08f1ac96d84166cb4e8eb
Felipe <felipesindeaux@outlook.com>
2022-03-23 14:32:05 -0300

    fix: cards

--- src/components/MainCards/index.jsx
+++ src/components/MainCards/index.jsx
@@ -3,23 +3,22 @@ import { Container } from "./styled";
 import { useContext } from "react";
 import { PendingsContext } from "../../Providers/Pendings";
 import { useState } from "react";
-import { Box, Grid, InfiniteScroll } from "grommet";
 
 const MainCards = ({
+  textContent,
   setOpen,
   alternative,
   arrayToRender,
   setServiceHired,
   setServiceID,
+  pending,
+  accepted,
 }) => {
- 
+
   return (
-    
     <Container>
-      <Grid columns="270px" overflow='auto' gap='large'>
-      <InfiniteScroll items={arrayToRender} step={1}>
       {
-        (item, index) => {
+        arrayToRender.map((item, index) => {
 
           const name = alternative ? item.hirerName : item.name;
 
@@ -33,20 +32,20 @@ const MainCards = ({
               id={item.id}
               desc={item.desc}
               setOpen={setOpen}
+              textContent={textContent}
               alternative={alternative}
               setServiceHired={setServiceHired}
               setServiceID={setServiceID}
+              pending={pending}
+              accepted={accepted}
               pendingId={item.pendingId}
               pendingStatus={item.pendingStatus}
               tag = {item.tags}
               userImage={item.userImage}
             />
           );
-        }}
-        </InfiniteScroll>
-      </Grid>
-    </Container>    
-
+        })}
+    </Container>
   );
 };
 
