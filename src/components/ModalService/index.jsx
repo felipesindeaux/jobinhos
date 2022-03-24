f32f2991b3ebf33b8523e54f07ba5fa8277d5b60
Felipe <felipesindeaux@outlook.com>
2022-03-24 19:03:01 -0300

    feat: noServicesButton

--- src/components/ModalService/index.jsx
+++ src/components/ModalService/index.jsx
@@ -8,21 +8,35 @@ import { useState } from "react";
 import { useContext } from "react";
 import { ServicesContext } from "../../Providers/Services";
 
-const ModalService = ({ setOpen, handleCloseModal, setShowModal }) => {
-  const closeModal = () => {
-    setOpen(false);
-  };
-
-  const { services } = useContext(ServicesContext);
+const ModalService = ({
+  setOpen,
+  handleCloseModal,
+  setShowModal,
+  isCreateService,
+}) => {
   const { hireService } = useContext(ServicesContext);
-  const { modifyService } = useContext(ServicesContext);
+  const { modifyService, createService } = useContext(ServicesContext);
   const [openName, setOpenName] = useState(false);
 
-  const formSchema = yup.object().shape({
-    title: yup.string().max(20, "Número máximo de caracteres é 20"),
-    price: yup.number(),
-    desc: yup.string().max(100, "Número máximo de caracteres é 100"),
-  });
+  const formSchema = isCreateService
+    ? yup.object().shape({
+        title: yup
+          .string()
+          .max(20, "Número máximo de caracteres é 20")
+          .required("Campo obrigatório"),
+        price: yup.number().required("Campo obrigatório"),
+        desc: yup
+          .string()
+          .max(100, "Número máximo de caracteres é 100")
+          .required("Campo obrigatório"),
+        images: yup.string().required("Campo obrigatório"),
+        tags: yup.string().required("Campo obrigatório"),
+      })
+    : yup.object().shape({
+        title: yup.string().max(20, "Número máximo de caracteres é 20"),
+        price: yup.number(),
+        desc: yup.string().max(100, "Número máximo de caracteres é 100"),
+      });
 
   const {
     register,
@@ -37,19 +51,27 @@ const ModalService = ({ setOpen, handleCloseModal, setShowModal }) => {
   };
 
   const onSubmitFunction = (dataService) => {
-    if (dataService.images === "") {
-      delete dataService.images;
+    if (isCreateService) {
+      dataService.images = dataService.images.split(" ");
+      dataService.tags = dataService.tags.split(" ");
+      createService(dataService)
+      handleCloseModal()
     } else {
-      dataService.images = [dataService.images];
+      if (dataService.images === "") {
+        delete dataService.images;
+      } else {
+        dataService.images = dataService.images.split(" ");
+      }
+      if (dataService.title === "") {
+        delete dataService.title;
+      }
+      if (dataService.desc === "") {
+        delete dataService.desc;
+      }
+      handleCloseModal();
+      putApiServices({ ...hireService[0], ...dataService });
     }
-    if (dataService.title === "") {
-      delete dataService.title;
-    }
-    handleCloseModal();
-    putApiServices({ ...hireService[0], ...dataService });
-
   };
-
 
   const onOpenName = () => setOpenName(true);
   const onCloseName = () => setOpenName(false);
@@ -74,12 +96,12 @@ const ModalService = ({ setOpen, handleCloseModal, setShowModal }) => {
             <Heading level={2} margin="none"></Heading>
             <Button icon={<Close />} onClick={() => setShowModal(false)} />
           </Box>
-          <FormField label="Serviço prestado">
-            <TextInput name="title"{...register("title")} />
+          <FormField label="Título">
+            <TextInput name="title" {...register("title")} />
           </FormField>
           <Errors>{errors.title?.message}</Errors>
 
-          <FormField label="Valor Serviço prestado">
+          <FormField label="Valor">
             <TextInput {...register("price")} type="number" min={0} icon="R$" />
           </FormField>
           <FormField label="Descrição">
@@ -87,7 +109,20 @@ const ModalService = ({ setOpen, handleCloseModal, setShowModal }) => {
           </FormField>
           <Errors>{errors.desc?.message}</Errors>
 
-          <FormField label="Imagens">
+          {isCreateService && (
+            <>
+              <FormField label="Tags (Separadas por espaços)">
+                <TextInput
+                  name="tags"
+                  {...register("tags")}
+                  placeholder="Adicione sua url"
+                />
+              </FormField>
+              <Errors>{errors.tags?.message}</Errors>
+            </>
+          )}
+
+          <FormField label="Imagens (Separadas por espaços)">
             <TextInput
               name="images"
               type="url"
@@ -95,6 +130,7 @@ const ModalService = ({ setOpen, handleCloseModal, setShowModal }) => {
               placeholder="Adicione sua url"
             />
           </FormField>
+          <Errors>{errors.images?.message}</Errors>
 
           <Button type="submit" label="Submit" primary />
         </Box>
